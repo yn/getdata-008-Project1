875f937ef90c62ce5a26fa5937481cd40c9c0763
Yuri Niyazov <yuri.niyazov@gmail.com>
2014-10-25 12:47:28 -0700

    Finish the code

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 #setwd("UCI HAR Dataset")
 library(data.table)
-#adapt the line below to load up datasets in one shot
 read_tables = function (files, cn) {
   data.table(rbindlist(lapply(files, read.table, col.names=cn)))
 }
@@ -9,11 +8,12 @@ read_RDSes = function(files) {
 }
 ty = read_tables(c("train/y_train.txt", "test/y_test.txt"), c("activity_id"))
 ts = read_tables(c("train/subject_train.txt", "test/subject_test.txt"), c("subject_id"))
-al = fread("activity_labels.txt")
+al = read_tables("activity_labels.txt", c("activity_id", "activity_name"))
 f = fread("features.txt")
 mean_std_cols = f[,grep("mean\\(\\)|std\\(\\)", V2)]
 
-tX = read_RDSes(c("trX.rds","teX.rds"))[,mean_std_cols,with=FALSE]
-setNames(tX, f[mean_std_cols, V2])
-#tX = read_tables(c("trX.rds","teX.rds"), f$V2)[,mean_std_cols,with=FALSE]
+#tX = read_RDSes(c("trX.rds","teX.rds"))[,mean_std_cols,with=FALSE]
+#setNames(tX, f[mean_std_cols, V2])
+tX = read_tables(c("train/X_train.txt","test/X_test.txt"), f$V2)[,mean_std_cols,with=FALSE]
 DF = cbind(tX, ty, ts)
+DF = merge(DF, al, by="activity_id")[,lapply(.SD,mean),by=c("subject_id", "activity_name")]
